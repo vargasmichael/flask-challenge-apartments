19c23d5b2c727c0005f0a2821457b07832ebc5c2
Adam La Rosa <adam.larosa@flatironschool.com>
2023-04-11 04:35:05 -0700

    flask restful import

--- server/models.py
+++ server/models.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 
 db = SQLAlchemy()
-
