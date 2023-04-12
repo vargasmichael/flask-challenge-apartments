ed2c19dd98aa6dfd4cba8751ba49e421c468dd2f
vargasmichael <119553981+vargasmichael@users.noreply.github.com>
2023-04-12 11:44:58 -0600

    full crud my dudes

--- server/app.py
+++ server/app.py
@@ -1,16 +1,153 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db
+from models import db, Apartment, Tenant, Lease
 
 app = Flask( __name__ )
 app.config[ 'SQLALCHEMY_DATABASE_URI' ] = 'sqlite:///apartments.db'
 app.config[ 'SQLALCHEMY_TRACK_MODIFICATIONS' ] = False
+app.json.compact = False
 
 migrate = Migrate( app, db )
+
 db.init_app( app )
+api = Api( app )
+
+class All_Apartments(Resource):
+    def get(self):
+        all_apartments = Apartment.query.all()
+        apartment_list = []
+        for apartment in all_apartments:
+            new_apartment = {
+                'id' : apartment.id,
+                'number' : apartment.number
+            }
+            apartment_list.append(new_apartment)
+
+    def post(self):
+        data = request.get_json()
+        apartment = Apartment(number=data['number'])
+        db.session.add(apartment)
+        db.session.commit()
+        return make_response("apartment added", 201)
+    
+api.add_resource(All_Apartments, '/apartments')       
+
+class Apartment_By_Id(Resource):
+    def get(self, id):
+        apartment = Apartment.query.filter_by(id=id).first()
+        if not apartment:
+            return make_response(jsonify({'error': 'Apartment not found'}), 404)
+        response = make_response(apartment.to_dict(), 200)
+        return response
+    
+    def patch(self, id):
+        apartment = Apartment.query.filter_by(id=id).first()
+        data = request.get_json()
+        for attr, value in data.items():
+            setattr(apartment, attr, value)
+        db.session.add(apartment)
+        db.session.commit()
+        
+        response = make_response(apartment.to_dict(), 200)
+        return response
+
+    def delete(self, id):
+        apartment = Apartment.query.filter_by(id=id).first()
+        if not apartment:
+            return make_response("apartment not found", 404)
+        db.session.delete(apartment)
+        db.session.commit()
+        return make_response("apartment deleted", 200)
 
 
+api.add_resource(Apartment_By_Id, '/apartments/<int:id>')
+
+
+
+class All_Tenants(Resource):
+    def get(self):
+        all_tenants = Tenant.query.all()
+        tenant_list = []
+        for tenant in all_tenants:
+            new_tenant = {
+                'id' : tenant.id,
+                'name' : tenant.name,
+                'age' : tenant.age
+            }
+            tenant_list.append(new_tenant)
+            
+    def post(self):
+        data = request.get_json()
+        tenant = Tenant(name=data['name'], age=request.get_json()['age'])
+        db.session.add(tenant)
+        db.session.commit()
+        return make_response(tenant.to_dict(), 201)
+    
+api.add_resource(All_Tenants, '/tenants')
+    
+class Tenant_By_Id(Resource):
+    def get(self, id):
+        tenant = Tenant.query.filter_by(id=id).first()
+        if not tenant:
+            return make_response(jsonify({'error': 'Tenant not found'}), 404)
+        response = make_response(tenant.to_dict(), 200)
+        return response
+    
+    def delete(self, id):
+        tenant = Tenant.query.filter_by(id=id).first()
+        if not tenant:
+            return make_response("tenant not found", 404)
+        db.session.delete(tenant)
+        db.session.commit()
+        return make_response("tenant deleted", 200)
+    
+    def patch(self, id):
+        tenant = Tenant.query.filter_by(id=id).first()
+        data = request.get_json()
+        for attr, value in data.items():
+            setattr(tenant, attr, value)
+        db.session.add(tenant)
+        db.session.commit()
+        
+        response = make_response(tenant.to_dict(), 200)
+        return response
+
+api.add_resource(Tenant_By_Id, '/tenants/<int:id>')
+
+class All_Leases(Resource):
+    def get(self):
+        all_leases = Lease.query.all()
+        lease_list = []
+        for lease in all_leases:
+            new_lease = {
+                'id' : lease.id,
+                'rent' : lease.rent,
+                'tenant_id' : lease.tenant_id,
+                'apartment_id' : lease.apartment_id
+            }
+            lease_list.append(new_lease)
+
+    def post(self):
+        data = request.get_json()
+        lease = Lease(rent=data['rent'], tenant_id=data['tenant_id'], apartment_id=data['apartment_id'])
+        db.session.add(lease)
+        db.session.commit()
+        return make_response("lease added", 201)
+    
+api.add_resource(All_Leases, '/leases')
+
+class Lease_By_Id(Resource):
+    def delete(self, id):
+        lease = Lease.query.filter_by(id=id).first()
+        if not lease:
+            return make_response("lease not found", 404)
+        db.session.delete(lease)
+        db.session.commit()
+        return make_response("lease deleted", 200)
+    
+
+api.add_resource(Lease_By_Id, '/leases/<int:id>')
 if __name__ == '__main__':
     app.run( port = 3000, debug = True )
--- server/models.py
+++ server/models.py
@@ -1,7 +1,57 @@
+#export FLASK_APP=app.py
+# export FLASK_RUN_PORT=5555
+# flask db init
+# flask db revision --autogenerate -m 'Create tables' 
+# flask db upgrade 
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, Resource
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy import MetaData
 
-db = SQLAlchemy()
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
+
+class Apartment(db.Model, SerializerMixin):
+    __tablename__ = 'apartments'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer)
+    
+
+class Tenant(db.Model, SerializerMixin):
+    __tablename__ = 'tenants'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    age = db.Column(db.Integer)
+    
+    @validates('name')
+    def validates_name(self, key, name):
+        if not name:
+            raise AssertionError('Name cannot be empty!')
+        return name
+    
+    @validates('age')
+    def validates_age(self, key, age):
+        if age <  18:
+            raise AssertionError('Age must be greater than 18!')
+        return age
+    
+class Lease(db.Model, SerializerMixin):
+    __tablename__ = 'leases'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    rent = db.Column(db.Integer)
+    tenant_id = db.Column(db.Integer, db.ForeignKey('tenants.id'))
+    apartment_id = db.Column(db.Integer, db.ForeignKey('apartments.id'))
+    
+    tenant = db.relationship('Tenant', backref='leases')
+    apartment = db.relationship('Apartment', backref='leases')
+    
+    serialize_rules = ('-apartments.leases', '-tenants.leases')
